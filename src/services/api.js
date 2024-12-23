709d29724f76a38834fa0aab8130a6c6666289da
Marina Ebbecke <m.ebbecke@gmail.com>
2024-12-23 19:23:08 -0300

    Módulo 7

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: import.meta.env.VITE_APP_API_BACKEND ?? "http://localhost:3333",
 })
-
-// Add a response interceptor
-api.interceptors.response.use(
-  (response) => {
-    // Simulate a delay
-    return new Promise((resolve) => setTimeout(() => resolve(response), 1000)) // 1 second delay
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
